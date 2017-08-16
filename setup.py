efde49e3aed6241f274282fb5499fe49bfb738ce
bengtl <bengt@ljungquistinfo>
2017-08-17 00:01:53 +0200

    deleting build

--- setup.py
+++ setup.py
@@ -11,24 +11,27 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
+extensions = [Extension("encoder", ["spikebit/encoder.pyx"]),
+              Extension("patterncheck", ["spikebit/patterncheck.pyx"])]
+
 setup(name='spikebit',
       description='Electrophysiology data integration',
       version='0.2',
       author='Bengt Ljungquist',
-      authour_email='bengt@ljungquist.info',
+#      authour_email='bengt@ljungquist.info',
       py_modules=['spikebit'],
-      install_requires=['Cython>=0.25.2',
-                    'h5py>=2.7',
-                    'mpi4py>=2.0',
-                    'numpy>=1.13.0'
-                    ],
+#      install_requires=['Cython>=0.25.2',
+                        'h5py>=2.7',
+                        'mpi4py>=2.0',
+                        'numpy>=1.13.0'
+                        ],
       platforms='linux',
       license='MIT',
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = [Extension("mandelcy", ["mandelcy.pyx"], )],
-      include_dirs = [numpy.get_include(),],
-      entry_points = {
-        'console_scripts': 
+      cmdclass={'build_ext': build_ext},
+      ext_modules=extensions,
+      include_dirs=[numpy.get_include(), ],
+#      entry_points={
+        'console_scripts':
             ['spikebit-server=spikebit.command_line:server_main',
             'spikebit-client=spikebit.command_line:client_main'],
             })
