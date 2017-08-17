6e442a5f3d1c3b9881811b9adc83e5374a99ac35
bengtl <bengt@ljungquistinfo>
2017-08-17 03:58:15 +0200

    changed check

--- spikebit/patterncheck.pyx
+++ spikebit/patterncheck.pyx
@@ -8,17 +8,14 @@ cimport numpy as np
 import gmpy2
 from gmpy2 import mpz
 
-def checkone(np.ndarray D, np.ndarray aPattern):
+def checkForPattern(np.ndarray D, np.ndarray aPattern):
     """ 
     patterncheck(D,aPattern) 
     Checks bitwise if bits of the required pattern aPattern are set in columns
-    of array D. Returns an array of booleans for each length of window
+    of array D. Returns an array of booleans for each bin of window
     """
     # TODO tile aPattern to same size as D 
     # check if equal with np.equal()
-    if 
-    winlength= D.shape[1]
-    
-    
-
-def checkPrevious()    
+    winSz = D.shape[1]    
+    patternMat = np.transpose(np.tile(aPattern,(winSz,1)))
+    return np.all(np.equal(D,patternMat), axis=0)
